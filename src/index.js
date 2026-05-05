d4761dfc1280f7b7d83ddb243bed747686a90748
Patrícia Moutinho <patdesigns@gmail.com>
2026-05-05 17:07:17 +0100

    changed some js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  iconElement.innerHTML = `<imgsrc="${response.data.condition.icon_url}" class="weather-app-icon" /> `;
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
+   cityElement.innerHTML = response.data.city;
+     timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = formatDate(date);
+
   temperatureElement.innerHTML = Math.round(temperature);
+
+   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" /> `;
+ getForecast(response.data.city)
+
+
 }
 
 function formatDate(date) {
@@ -37,7 +43,7 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  return `${day} ${hours} ${minutes} `;
+  return `${day} ${hours}:${minutes} `;
 }
 
 function searchCity(city) {
